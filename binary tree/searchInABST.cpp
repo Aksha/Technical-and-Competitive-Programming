631ea706f9b86b452be8ab73aefa9a6db50809b3
Akshaya Ravichandran <Aksha@users.noreply.github.com>
2019-12-27 19:28:41 -0800

    Update searchInABST.cpp

--- binary tree/searchInABST.cpp
+++ binary tree/searchInABST.cpp
@@ -47,3 +47,12 @@ public:
         }
     }
 };
+
+concise iterative solution:
+
+TreeNode* searchBST(TreeNode* root, int val) {
+    while (root != nullptr && root->val != val) {
+      root = (root->val > val) ? root->left : root->right;
+    }
+    return root;
+}
