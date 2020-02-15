43079120212d291f648476178f9b821e2485ef93
Akshaya Ravichandran <Aksha@users.noreply.github.com>
2020-02-15 11:14:08 -0800

    Same leaf nodes in both trees

--- binary tree/sameLeafNodes.cpp
+++ binary tree/sameLeafNodes.cpp
@@ -1,6 +1,6 @@
 //Write a program to find out if two trees have the same leaf nodes
 
-//Idea: Tree Iterator
+/*Idea: Tree Iterator
 first tree:
 
       a                   
@@ -15,7 +15,39 @@ second tree:
      / \
     b   f
    / \   \
-  c   d   e
+  c   d   e */
   
   
 //  Solution: 
+
+struct Node {
+      int val;
+      Node* left;
+      Node* right;
+      Node(int v) : val(v), left(NULL), right(NULL) {}
+}
+
+struct iterator {
+      iterator* next();
+      bool hasNext();
+}
+
+struct leafNodeIterator : iterator {
+      leafNodeIterator(Node n) {}
+}
+
+//Some magical method for understanding: 
+
+bool isEqualLeafNodes(Node tree1, Node tree2) {
+      iterator <Node> it1 = new leafNodeIterator(tree1);
+      iterator <Node> it2 = new leafNodeIterator(tree2);
+      
+      while(it1.hasNext() && it2.hasNext()) {
+            Node leaf1 = it1.next();
+            Node leaf2 = it2.next();
+            if(leaf1.val != leaf2.val) return false;
+      }
+      return !it1.hasNext() && !it2.hasNext();
+}
+
+//Actual Implementation: 
