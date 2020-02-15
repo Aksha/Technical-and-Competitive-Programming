04105caf0175507ddd6565a8c195095a1ff24139
Akshaya Ravichandran <Aksha@users.noreply.github.com>
2020-02-15 10:54:09 -0800

    Create sameLeafNodes.cpp

---
+++ binary tree/sameLeafNodes.cpp
@@ -0,0 +1,21 @@
+//Write a program to find out if two trees have the same leaf nodes
+
+//Idea: Tree Iterator
+first tree:
+
+      a                   
+     / \
+    b   e
+   / \
+  c   d
+  
+second tree:
+  
+      a
+     / \
+    b   f
+   / \   \
+  c   d   e
+  
+  
+//  Solution: 
