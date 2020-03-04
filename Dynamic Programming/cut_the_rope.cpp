2a7b6667de864a282699daf9a9fd54b448b04258
Akshaya Ravichandran <Aksha@users.noreply.github.com>
2020-03-03 18:48:19 -0800

    Update cut_the_rope.cpp

--- Dynamic Programming/cut_the_rope.cpp
+++ Dynamic Programming/cut_the_rope.cpp
@@ -1,4 +1,4 @@
-Flawed solution:
+//Flawed solution:
 
 /*
  * Complete the max_product_from_cut_pieces function below.
@@ -21,5 +21,29 @@ long long int max_product_from_cut_pieces(int n) {
     }
     return dp[n];
 }
+
+//Analyze ^^ why because it is considering only a product of two numbers. The rope could be cut into more pieces.
+
+/*
+ * Complete the max_product_from_cut_pieces function below.
+ */
+long long int max_product_from_cut_pieces(int n) {
+    /*
+     * Write your code here.
+     */
+    long long int dp[n+1];
+    dp[0] = 0;
+    dp[1] = 1;
+    for(long long int i = 2; i <= n; ++i) {
+        long long int maxProduct = LLONG_MIN;
+        for(long long int j = 1; j <= i/2; ++j) {
+            long long int tempMax = max((i-j)*j, j*dp[i-j]);
+            maxProduct = max(maxProduct, tempMax); 
+        }
+        dp[i] = maxProduct; 
+    }
+    return dp[n];
+}
+
 
 
