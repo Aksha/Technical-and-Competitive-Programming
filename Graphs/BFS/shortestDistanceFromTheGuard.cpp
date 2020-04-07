164ddc215c1ff2b56e38f2c1acf401ca1a8a1387
Akshaya Ravichandran <Aksha@users.noreply.github.com>
2020-04-06 20:15:01 -0700

    Update shortestDistanceFromTheGuard.cpp

--- Graphs/BFS/shortestDistanceFromTheGuard.cpp
+++ Graphs/BFS/shortestDistanceFromTheGuard.cpp
@@ -116,8 +116,6 @@ Constraints:
 
 
 */
-
-vector<vector<int>> find_shortest_distance_from_a_guard(vector<vector<char>> grid) {
 /*
 case 1 : if(cellNum == G) cellNum = 0;
 case 2 : else if (cellNum == W) cellNum = -1;
@@ -130,4 +128,70 @@ Better approach: Start from 'G' and find the values. Stop when you encounter ano
 Best Approach : Put all the Gs in the queue and apply BFS. 
 */
 
+struct GaurdNode
+{
+	int x;
+	int y;
+	int distance;
+};
+
+bool isValid(vector<vector<char>>& grid, int i, int j)
+{
+	return (i >= 0 && i < grid.size()) && (j >= 0 && j < grid[0].size());
 }
+
+bool isSafe(int i, int j, vector<vector<char>>& grid, vector<vector<int>>& result)
+{
+	return grid[i][j] == 'O' && result[i][j] == -1;
+}
+
+void UpdateDistance(vector<vector<char>>& grid, vector<vector<int>>& result)
+{
+	int x, y, distance;
+	GaurdNode node;
+	queue<GaurdNode> Q;
+	int R[] = { 0, -1, 0, 1 };
+	int C[] = { -1, 0, 1, 0 };
+
+
+	for (int i = 0; i < grid.size(); i++) {
+		for (int j = 0; j < grid[0].size(); j++) {
+			if (grid[i][j] == 'G') {
+				Q.push({i, j, 0});
+
+				result[i].push_back(0);
+				continue;
+			}
+			result[i].push_back(-1);
+		}
+	}
+
+	while (!Q.empty()) {
+		x = Q.front().x;
+		y = Q.front().y;
+		distance = Q.front().distance;
+
+		Q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			node.x = x + R[i], node.y = y + C[i];
+
+			if (isValid(grid, node.x, node.y)
+			&& 	isSafe(node.x, node.y, grid, result)) {
+				result[node.x][node.y] = distance + 1;
+				Q.push({node.x, node.y, distance + 1});
+			}
+		}
+	}
+}
+
+vector<vector<int>> find_shortest_distance_from_a_guard(vector<vector<char>>& grid) {
+
+	vector<vector<int> > result;
+
+	int size =  grid.size();
+	result.resize(size);
+	UpdateDistance(grid, result);
+	return result;
+}
+
