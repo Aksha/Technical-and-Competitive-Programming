7951e2ba7881e7855c66acdcf83e983576b8ef69
Akshaya Ravichandran <Aksha@users.noreply.github.com>
2020-04-19 09:15:12 -0700

    Work in Progress

--- Recursion new/expressionToTarget.cpp
+++ Recursion new/expressionToTarget.cpp
@@ -133,3 +133,120 @@ vector <string> generate_all_expressions(string s, long long int target) {
     return result;
 }
 
+//Better way:
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+//All expressions that sum to the target value
+/*
+* Input:
+s="222", target=24.
+Output:
+
+["22+2", "2+22"] and ["2+22", "22+2"] are both correct outputs.
+
+22+2=24: we inserted the “join” operator between the first two characters and the “+” operator between the last two characters of s.
+2+22=24: we inserted the “+” operator between the first two characters and the “join” operator between the last two characters of s.
+
+ This problem has two parts.. Getting the expressions string and then calculating its value to check if evaluates to the target.
+ * */
+
+void generate_all_expressions_helper(string s, int index, string stringSoFar, vector<string>& result, int n) {
+    if(index == n) {
+        result.push_back(stringSoFar);
+        return;
+    }
+    generate_all_expressions_helper(s,index+1,stringSoFar + "+" + s[index], result, n);
+    generate_all_expressions_helper(s,index+1,stringSoFar + "*" + s[index], result, n);
+    generate_all_expressions_helper(s,index+1,stringSoFar + "|" + s[index], result, n);
+    return;
+}
+
+vector<string> generate_all_expressions(string s) {
+    int index = 1;
+    vector<string> result; // collects all strings
+    string stringSoFar = "";
+    stringSoFar += s[0];
+    int n = s.length();
+    generate_all_expressions_helper(s,index,stringSoFar,result,n);
+    return result;
+}
+
+int calcVal(string s) {
+    bool mul = 0;
+    bool add = 0;
+    bool concat = 0;
+    int i = 0;
+    int n = s.size();
+    int num = 0;
+    int value = 0;
+    while(i < n) {
+        if(s[i] == '+')
+            add = 1;
+        else if(s[i] == '*')
+            mul = 1;
+        else if(s[i] == '|')
+            num = num * 10 + (s[i] - '0');
+        else if(add == 1) {
+            int temp = 0;
+            while(s[i] != '+' && s[i] != '*') {
+                if(temp == 0) {
+                    temp = (s[i]-'0');
+                }
+                else {
+                    temp = temp*10 + (s[i] - '0');
+                }
+                i++;
+            }
+            value += num + temp;
+            num = 0;
+            add = 0;
+        }
+        else if(mul != 0) {
+            int temp = 0;
+            while(s[i] != '+' && s[i] != '*') {
+                if(temp == 0) {
+                    temp = (s[i]-'0');
+                }
+                else {
+                    temp = temp*10 + (s[i] - '0');
+                }
+                i++;
+            }
+            value += num * temp;
+            num = 0;
+            mul = 0;
+        }
+        else
+            num = s[i];
+        i++;
+    }
+    return value;
+}
+
+int main() {
+    string s = "222";
+    long long int target = 24;
+    vector<string> expressions = generate_all_expressions(s);
+    cout << "Printing the generated strings: " << endl;
+    int n = expressions.size();
+    vector<string> result;
+    for(int i = 0; i < n; i++) {
+        int value = calcVal(expressions[i]);
+        if(value == target) {
+            result.push_back(expressions[i]);
+        }
+    }
+    //calculate values at this point
+    for(int i = 0; i < result.size(); i++) {
+        cout << result[i] << endl;
+    }
+    return 0;
+}
+
+
