ec04045704965b7b40063c3d96aee41690202570
Akshaya Ravichandran <Aksha@users.noreply.github.com>
2020-06-05 16:17:40 -0700

    Update number_of_ways_to_reach_target.cpp

--- Dynamic Programming/combinations/number_of_ways_to_reach_target.cpp
+++ Dynamic Programming/combinations/number_of_ways_to_reach_target.cpp
@@ -37,4 +37,32 @@ int main() {
 }
 
 //Now, Dynamic programming approach: counting problems and optimation problems can be solved using DP (top down).
+//Time complexity is O(m*n) where m is the target and n is the number of elements in the input. This can be better or worse than
+// recursion depending on the values of m and n but stack overhead is not there and at the system level,
+//iteration is faster.
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int count_combinations(vector<int> arr, int target) {
+    int size = arr.size();
+    vector<int> count(size+1, 0);
+    count[0] = 1; // base case
+    for(int i = 0; i <= target; i++) {
+        for(int j = 0; j < size; j++) {
+            if(i >= arr[j]) {
+                count[i] += count[i-arr[j]];
+            }
+        }
+    }
+    return count[target];
+}
+
+int main() {
+    vector<int> arr = {2,3,7};
+    int target = 5;
+    int result = count_combinations(arr,target);
+    cout << "the total number of ways : " << result << endl;
+    return 0;
+}
