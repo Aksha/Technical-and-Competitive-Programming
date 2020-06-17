e508bacec8263b941fe413647a4c53a4c18bf326
Akshaya Ravichandran <Aksha@users.noreply.github.com>
2020-06-17 12:26:48 -0700

    WIP

---
+++ Dynamic Programming/longest common subsequence.cpp
@@ -0,0 +1,17 @@
+/*
+Longest Common Subsequence
+
+Find the longest common subsequence of two strings.
+A subsequence is a sequence that can be derived from another sequence by deleting zero or more elements without changing the order of the remaining elements.
+
+Example
+Input: “ABCDE”, “AECBD”
+Output: “ABD”
+Subsequence “ABD” can be derived from the first string by deleting characters “C” and “E”. From the second string it can be derived by deleting “E” and “C”. No common subsequence longer than three characters exists in the two given strings. “ACD” is another common subsequence of length three; it is also a correct answer.
+
+Notes
+
+Input Parameters: The function has two string parameters.
+Output: Return a string, the longest common subsequence. If a nonempty common subsequence cannot be found, return “-1”.
+*/
+
