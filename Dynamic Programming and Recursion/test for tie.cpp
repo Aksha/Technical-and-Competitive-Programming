3d7f1e909f50ab322057fda0c286baa3efb7f23d
Akshaya Ravichandran <Aksha@users.noreply.github.com>
2020-06-18 16:26:11 -0700

    Update test for tie.cpp

--- Dynamic Programming and Recursion/test for tie.cpp
+++ Dynamic Programming and Recursion/test for tie.cpp
@@ -13,28 +13,41 @@ room 2 people = {4,7} (weight = 11)
 
 //Recursive Implementation: O(2^n) where n is the number of people
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 bool testfortie_helper (vector<int> arr, int i, int halfSum, bool result){
-  if(halfSum == 0){
-    return true;
-  }
-  else if(halfSum < 0) {
-    return false;
-  }
-  else if(halfSum - arr[i] >= 0) {
-     result = testfortie_helper(arr,i, halfSum - arr[i], result);
-  }
-  return result || testfortie_helper(arr, i, halfSum, result);
+    if(halfSum == 0)
+        return true;
+    if(i == arr.size())
+        return false;
+    if(halfSum < 0)
+        return false;
+    else if(halfSum - arr[i] >= 0)
+        result = testfortie_helper(arr,i+1, halfSum - arr[i], result);
+    return result || testfortie_helper(arr, i+1, halfSum, result);
 }
 
 bool testfortie(vector<int> arr) {
-  int sum = 0;
-  for(int i = 0; i < arr.size(); i++) {
-    sum += arr[i];
-  }
-  if(sum % 2 != 0)
-    return false;
-  int halfSum = sum/2;
-  bool result = false;
-  int i = 0;
-  return testfortie_helper(arr,i, halfSum, result);
+    int sum = 0;
+    for(int i = 0; i < arr.size(); i++) {
+        sum += arr[i];
+    }
+    if(sum % 2 != 0)
+        return false;
+    int halfSum = sum/2;
+    bool result = false;
+    int i = 0;
+    return testfortie_helper(arr,i, halfSum, result);
+}
+
+int main() {
+    vector <int> arr = {56,2};
+    if(arr.size() == 0 || arr.size() == 1)
+        return false;
+    bool result = testfortie(arr);
+    cout << "The partition exists? :  " << result << endl;
+    return 0;
 }
