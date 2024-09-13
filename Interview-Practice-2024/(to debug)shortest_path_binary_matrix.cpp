d0237a3e71ceb9efb285c1990e2a0f993a0e4f6a
Akshaya Ravichandran <akshaya89@gmail.com>
2024-09-13 00:25:22 -0700

    Update (to debug)shortest_path_binary_matrix.cpp

--- Interview-Practice-2024/(to debug)shortest_path_binary_matrix.cpp
+++ Interview-Practice-2024/(to debug)shortest_path_binary_matrix.cpp
@@ -59,3 +59,48 @@ public:
 //if you reach the end of the matrix, return distance
 //if you don't, keep going over the other elements in the matrix.
 //if we reach the end without finding a path, return -1;
+
+
+Working solution: 
+
+class Solution {
+public:
+
+    bool boundary_conditions(int r, int c, int m, int n) {
+        if(c < 0 || c >= n || r < 0 || r >= m)
+            return false;
+        return true;
+    }
+
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        if(grid[0][0] != 0 || grid[n-1][m-1] != 0) return -1;
+        vector<vector<bool>> visited(m, vector<bool>(n,0));
+        vector<vector<int>> distance(m, vector<int>(n,0));
+        queue <pair<int,int>> Q;
+        Q.push(make_pair(0,0));
+        visited[0][0] = 1;
+        distance[0][0] = 1;
+        vector<pair<int,int>> directions = {{1,0},{0,1},{-1,0},{0,-1},{-1,-1},{1,1},{-1,1},{1,-1}};
+        while(!Q.empty()) {
+            int row = Q.front().first;
+            int col = Q.front().second;
+            Q.pop();
+            if(row == m - 1 && col == n -1) {
+                return distance[row][col];
+            }
+            for(int k = 0; k < directions.size(); k++) {
+                int new_row = directions[k].first + row;
+                int new_col = directions[k].second + col;
+                if(boundary_conditions(new_row, new_col, m, n) && !visited[new_row][new_col] && grid[new_row][new_col] == 0) {
+                    distance[new_row][new_col] = distance[row][col] + 1;
+                    visited[new_row][new_col] = 1;
+                    Q.push(make_pair(new_row, new_col));
+                }
+            }
+        }
+        return -1;
+    }
+};
+
