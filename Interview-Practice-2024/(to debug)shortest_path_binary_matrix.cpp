724a5837e1b920afc320eb538de053a6ea5b8d50
Akshaya Ravichandran <akshaya89@gmail.com>
2024-09-13 00:36:09 -0700

    Update (to debug)shortest_path_binary_matrix.cpp

--- Interview-Practice-2024/(to debug)shortest_path_binary_matrix.cpp
+++ Interview-Practice-2024/(to debug)shortest_path_binary_matrix.cpp
@@ -63,31 +63,28 @@ public:
 
 Working solution: 
 
-class Solution {
-public:
+bool boundary_conditions(int r, int c, int m, int n) {
+    if(c < 0 || c >= n || r < 0 || r >= m)
+        return false;
+    return true;
+}
 
-    bool boundary_conditions(int r, int c, int m, int n) {
-        if(c < 0 || c >= n || r < 0 || r >= m)
-            return false;
-        return true;
-    }
 
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+int shortest_path_length(vector<vector<int>> &grid) {
         int m = grid.size();
         int n = grid[0].size();
-        if(grid[0][0] != 0 || grid[n-1][m-1] != 0) return -1;
+        if(grid[0][0] != 0 || grid[m-1][n-1] != 0) return -1;
         vector<vector<bool>> visited(m, vector<bool>(n,0));
-        vector<vector<int>> distance(m, vector<int>(n,0));
+        vector<vector<int>> distance(m, vector<int>(n,1));
         queue <pair<int,int>> Q;
         Q.push(make_pair(0,0));
         visited[0][0] = 1;
-        distance[0][0] = 1;
         vector<pair<int,int>> directions = {{1,0},{0,1},{-1,0},{0,-1},{-1,-1},{1,1},{-1,1},{1,-1}};
         while(!Q.empty()) {
             int row = Q.front().first;
             int col = Q.front().second;
             Q.pop();
-            if(row == m - 1 && col == n -1) {
+            if(row == m-1 && col == n-1) {
                 return distance[row][col];
             }
             for(int k = 0; k < directions.size(); k++) {
@@ -101,6 +98,12 @@ public:
             }
         }
         return -1;
-    }
-};
+}
 
+//Shortest path can be found by BFS
+//if 0, start looking in all 8 directions. 
+//each element in the matrix should have a distance and visited variable associated with it
+//if not visited and it is 0 and in bounds, add it to the Q, increment distance = curr_distance + 1 and visited is set to true, add it to the Q and move on to the next.
+//if you reach the end of the matrix, return distance
+//if you don't, keep going over the other elements in the matrix.
+//if we reach the end without finding a path, return -1;
